9913de54d411bdea4ecc79a050954a3819202498
Sam Clercky <sam.clercky@gmail.com>
2024-06-19 14:27:38 +0200

    add --failed option

--- src/main.rs
+++ src/main.rs
@@ -4,19 +4,28 @@ fn main() -> Result<(), isize> {
     let mut cmd = std::env::args();
     cmd.next();
     let cmd = cmd.collect::<Vec<_>>();
+    // Extra argument that restarts the application only when the application has failed
+    let failed = cmd
+        .iter()
+        .next()
+        .map(|arg| *arg == "--failed")
+        .unwrap_or(false);
 
     let Some(proc) = cmd.first().cloned() else {
-        println!("You need to provide a valid program: keep-alive <program>");
+        println!("You need to provide a valid program: keep-alive [--failed] <program>");
         return Err(-1);
     };
     let mut args = cmd.iter().cloned();
     args.next();
+    if failed {
+        args.next();
+    }
     let args = args.collect::<Vec<_>>();
 
     println!("Keeping alive command: {:?}", cmd);
 
     while let Ok(output) = Command::new(&proc).args(&args).output() {
-        if output.status.success() {
+        if failed && output.status.success() {
             return Ok(());
         }
 
